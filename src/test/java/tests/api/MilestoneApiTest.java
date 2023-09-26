3372b524bbefaee8362e2017bff1df8011193594
Natalya Brago <nata_9507@mail.ru>
2023-09-27 01:01:08 +0300

    apiTests_hw

---
+++ src/test/java/tests/api/MilestoneApiTest.java
@@ -0,0 +1,91 @@
+package tests.api;
+
+import baseEntities.BaseApiTest;
+import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import models.Milestone;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+import utils.Endpoints;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class MilestoneApiTest extends BaseApiTest {
+    private final int projectId = 9;
+
+    @Test
+    public void getMilestone(){
+        RequestSpecification mRequestSpecification = given()
+                .pathParam("project_id",projectId)
+                .when();
+
+        Response response = mRequestSpecification
+                .get(Endpoints.GET_MILESTONES_ENDPOINT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void addMilestone() {
+        Milestone mMilestone = Milestone.builder()
+                .name("Milestone_for_project_id = " + projectId)
+                .description("Description for Milestone_for_project_id = " + projectId)
+                .build();
+
+        RequestSpecification mRequestSpecification = given()
+                .body(mMilestone, ObjectMapperType.GSON)
+                .pathParam("project_id", projectId)
+                .when();
+
+        Response response = mRequestSpecification.post(Endpoints.ADD_MILESTONE_ENDPOINT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("name", is(mMilestone.getName()))
+                .body("description", is(mMilestone.getDescription()))
+                .extract()
+                .response();
+    }
+
+
+    @Test
+    public void updateMilestone(){
+        int milestoneId = 9;
+
+        Milestone mMilestone = Milestone.builder()
+                .name("Update Milestone_for_project_id = " + projectId)
+                .description("Update Description for Milestone_for_project_id = " + projectId)
+                .build();
+
+        RequestSpecification mRequestSpecification = given()
+                .body(mMilestone, ObjectMapperType.GSON)
+                .pathParam("milestone_id",milestoneId)
+                .when();
+
+
+        Response response = mRequestSpecification.post(Endpoints.UPDATE_MILESTONE_ENDPOINT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("name",is(mMilestone.getName()))
+                .body("description",is(mMilestone.getDescription()))
+                .extract()
+                .response();
+    }
+
+   @Test
+    public void deleteMilestone(){
+        int milestoneId = 8;
+                given()
+                .pathParam("milestone_id",milestoneId)
+                .when()
+                .post(Endpoints.DELETE_MILESTONE_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+}
--- src/test/java/utils/Endpoints.java
+++ src/test/java/utils/Endpoints.java
@@ -1,8 +1,19 @@
 package utils;
 
 public class Endpoints {
+    public static final String ADD_MILESTONE_ENDPOINT = "/index.php?/api/v2/add_milestone/{project_id}";
+    public static final String GET_MILESTONES_ENDPOINT = "/index.php?/api/v2/get_milestones/{project_id}";
+    public static final String UPDATE_MILESTONE_ENDPOINT = "/index.php?/api/v2/update_milestone/{milestone_id}";
+    public static final String DELETE_MILESTONE_ENDPOINT  = "/index.php?/api/v2/delete_milestone/{milestone_id}";
+
+    public static final String ADD_CASE_ENDPOINT = "/index.php?/cases/add/{section_id}";
+    public static final String GET_CASE_ENDPOINT = "/index.php?/api/v2/get_case/{case_id}";
+    public static final String UPDATE_CASE_ENDPOINT = "/index.php?/api/v2/update_case/{case_id}";
+    public static final String DELETE_CASE_ENDPOINT = "/index.php?/api/v2/delete_case/{case_id}";
+
     public static final String ADD_PROJECT = "/index.php?/api/v2/add_project";
     public static final String GET_ALL_PROJECTS = "/index.php?/api/v2/get_projects";
     public static final String GET_ALL_USERS = "/index.php?/api/v2/get_users";
     public static final String GET_USER = "index.php?/api/v2/get_user/{user_id}";
+
 }
